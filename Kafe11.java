d65da07da69a003c7f9caffe00e71951760996b6
firdauzynuzula <you@example.com>
2025-11-27 11:31:17 +0700

    menambahkan beberapa file

---
+++ Kafe11.java
@@ -0,0 +1,5 @@
+public class Kafe11 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Kubus11.java
@@ -0,0 +1,6 @@
+public class Kubus11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
