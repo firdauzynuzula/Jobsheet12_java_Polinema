a1803777d5ea00406a8933061b7b5ea7c38f2da2
firdauzynuzula <you@example.com>
2025-11-27 14:56:48 +0700

    melakukan percobaan 2 dengan memodifikasi Kafe11.java

--- Kafe11.java
+++ Kafe11.java
@@ -1,20 +1,14 @@
 public class Kafe11 {
 
-    // public static void Menu() {
-    //     System.out.println("===== MENU RESTO KAFE ====");
-    //     System.out.println("1. Kopi Hitam - Rp 15,000");
-    //     System.out.println("2. Cappuccino - Rp 20,000");
-    //     System.out.println("3. Latte - Rp 22,000");
-    //     System.out.println("4. Teh Tarik - Rp 12,000");
-    //     System.out.println("5. Roti Bakar - Rp 10,000");
-    //     System.out.println("6. Mie Goreng - Rp 18,000");
-    //     System.out.println("==========================");
-    //     System.out.println("Silahkan pilih menu yang Anda inginkan.");
-    // }
+    public static void Menu(String namaPelanggan, boolean isMember) {
+      System.out.println("Selamat datang, " + namaPelanggan + "!");
 
-    public static void main(String[] args) {
-        // Menu();
-          System.out.println("===== MENU RESTO KAFE ====");
+      if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+      }
+
+
+        System.out.println("===== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
@@ -23,5 +17,9 @@ public class Kafe11 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("==========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    }
+
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
