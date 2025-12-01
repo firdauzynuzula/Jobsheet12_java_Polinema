5c909c629589c0104e24c15aa6a92c7ec9791262
firdauzynuzula <you@example.com>
2025-12-01 18:13:25 +0700

    memodifikasi file Kubus11.java

--- Kubus11.java
+++ Kubus11.java
@@ -1,5 +1,13 @@
 public class Kubus11 {
     public static void main(String[] args) {
-        
+        int sisi;
+        sisi = 7;
+        int volume = sisi * sisi * sisi;
+
+
+        System.out.println("Volume kubus dengan sisi " + sisi + " adalah " + volume);
+
+        int luasPermukaan = 6 * sisi * sisi;
+        System.out.println("Luas permukaan kubus dengan sisi " + sisi + " adalah " + luasPermukaan);
     }
 }
