35729f763e7ea5b3f0b77aea8db59fd5e6559449
firdauzy <nuzulafirdhauzy@gmail.com>
2025-12-03 09:12:10 +0700

    memodifikasi file Kubus11.java

--- Kubus11.java
+++ Kubus11.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class Kubus11 {
-    
+    public static int hitungVolume(int sisi) {
+        int volume = sisi * sisi * sisi;
+        return volume;
+    }
+
+    public static int hitungLuasPermukaan(int sisi) {
+        int luasPermukaan = 6 * sisi * sisi;
+        return luasPermukaan;
+    }
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -8,12 +16,10 @@ public class Kubus11 {
         System.out.print("Masukkan panjang sisi kubus: ");
         int sisi = input.nextInt();
         System.out.flush();
-        int volume = sisi * sisi * sisi;
-
-
+        int volume = hitungVolume(sisi);
         System.out.println("Volume kubus dengan sisi " + sisi + " adalah " + volume);
 
-        int luasPermukaan = 6 * sisi * sisi;
+        int luasPermukaan = hitungLuasPermukaan(sisi);
         System.out.println("Luas permukaan kubus dengan sisi " + sisi + " adalah " + luasPermukaan);
 
         input.close();
